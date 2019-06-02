61c43e6ee66558854df6111a631f866249b75215
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-06-02 11:16:00 +0200

    fuck you

--- scriptPanji.js
+++ scriptPanji.js
@@ -1,14 +1,15 @@
 var pageTitle = document.title
+var config = {
+		apiKey: "AIzaSyACSgbAIM01KGI_twhGzX1FABWyhhCS3Eo",
+		authDomain: "test-6ee2a.firebaseapp.com",
+		databaseURL: "https://test-6ee2a.firebaseio.com",
+		projectId: "test-6ee2a",
+		storageBucket: "test-6ee2a.appspot.com",
+		messagingSenderId: "448582725279"
+	}
+firebase.initializeApp(config)
 function submitToFirebase () {
-	var config = {
-			apiKey: "AIzaSyACSgbAIM01KGI_twhGzX1FABWyhhCS3Eo",
-			authDomain: "test-6ee2a.firebaseapp.com",
-			databaseURL: "https://test-6ee2a.firebaseio.com",
-			projectId: "test-6ee2a",
-			storageBucket: "test-6ee2a.appspot.com",
-			messagingSenderId: "448582725279"
-		}
-	firebase.initializeApp(config)
+	
 	var firebaseRef = firebase.database().ref('GoogleAnsewres/')
 	var  queArr = pageTitle.split('-')
 	var question = queArr[0]
